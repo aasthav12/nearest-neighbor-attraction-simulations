f16cdc8f405e297a3271391993db53e6e8b22425
aasthav12 <120447854+aasthav12@users.noreply.github.com>
2025-03-24 02:19:08 -0700

    adding p1

---
+++ part1/webWorker.js
@@ -0,0 +1,46 @@
+// Perform computation when receive data from main.js
+self.onmessage = function(event) {
+    
+    // Unpack data received from main.js
+    var NUMPARTICLES = event.data.NUMPARTICLES;
+    var particlesComputeData = new Float32Array(event.data.particlesComputeBuffer);
+    var particlesRenderData = new Float32Array(event.data.particlesRenderBuffer);
+
+    for (let i = 0; i < NUMPARTICLES; i++) {
+        
+        /* ADD YOUR COMPUTATION HERE*/
+        var neighbor_distance = Infinity;
+        var coor = -1;
+
+        for (let j = 0; j < NUMPARTICLES; j++){
+            if (i === j){
+                continue;
+            }
+            else {
+                var x = particlesComputeData[2*j] - particlesComputeData[2*i];
+                var y = particlesComputeData[2*j+1] - particlesComputeData[2*i+1];
+                var distance = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
+                if (distance < neighbor_distance && distance > 3) {
+                    neighbor_distance = distance;
+                    coor = j;
+                }
+            }
+        }
+        // update the positions
+        if (coor > -1){
+            var x = 1;
+            var y = 1;
+            if(particlesComputeData[2 * i] > particlesComputeData[2 * coor]){
+                x = -1;
+            }
+            if(particlesComputeData[2 * i + 1] > particlesComputeData[2 * coor + 1]){
+                y = -1;
+            }
+            particlesRenderData[2 * i] = particlesComputeData[2 * i] + x;
+            particlesRenderData[2 * i + 1] = particlesComputeData[2 * i + 1] + y;
+        }
+    }
+    
+    // Send back new data back to main.js
+    postMessage(null);
+}
